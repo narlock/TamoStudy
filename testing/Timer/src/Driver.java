82f565a416f189cd0856e11750173acdaa9db6ab
Anthony Narlock <anthonynarlock@outlook.com>
2021-03-08 10:41:45 -0600

    v0.1 indev part2
    added scanner for input (works).. going tmo make a test GUI for stopping in the middle of a focus session

--- testing/Timer/src/Driver.java
+++ testing/Timer/src/Driver.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args) {
-		int seconds = 60;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the amount of seconds to focus");
+		int seconds = input.nextInt();
 		
 		Countdown test = new Countdown(seconds);
 		test.timer.schedule(test.task, 1000, 1000);
