71193f367510683edc0415eb5ded1f9ef0cc4288
narlock <anthonynarlock@outlook.com>
2022-04-03 19:13:02 -0500

    Directory Updates + Dev Branch

---
+++ DEV/Software/JPanelGUIChanger/src/Runner.java
@@ -0,0 +1,5 @@
+public class Runner {
+	public static void main(String[] args) {
+		MainGUI gui = new MainGUI();
+	}
+}
---
+++ DEV/Software/JPanelGUIChanger/src/StateStrategy.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+/**
+ * StateStrategy
+ * @author Anthony Narlock
+ * @brief Abstract Implementation of the StateStrategy JPanel
+ */
+
+public abstract class StateStrategy extends JPanel {
+	public StateStrategy() {
+		setPanel();
+		setActions();
+	}
+	
+	//Sets the panel information
+	public abstract void setPanel();
+	
+	//Sets the actions of the panel
+	public abstract void setActions();
+	
+}
