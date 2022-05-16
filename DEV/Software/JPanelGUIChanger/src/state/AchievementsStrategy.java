e2b3351658792c79afed64e3ece8544ec7366353
narlock <anthonynarlock@outlook.com>
2022-05-16 14:45:27 -0500

    b4.0 achievement unlock

--- DEV/Software/JPanelGUIChanger/src/state/AchievementsStrategy.java
+++ DEV/Software/JPanelGUIChanger/src/state/AchievementsStrategy.java
@@ -19,7 +19,12 @@ public class AchievementsStrategy extends StateStrategy {
 	public AchievementsStrategy(Profile profile) {
 		super(profile);
 		// TODO Auto-generated constructor stub
+		earnedAchievements = profile.getAhmString().split("");
+		
+		setAchievements();
 	}
+	
+	private String[] earnedAchievements;
 
 	//Header
 	private JLabel achievementsHeaderLabel;
@@ -43,53 +48,6 @@ public class AchievementsStrategy extends StateStrategy {
 		
 		this.add(createSpaceLabel(0));
 		
-		hoursPanel = new JPanel();
-		setPanelComponent(hoursPanel);
-		hoursPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[0], profile.getLanguage().ahmText[0], "AHM_1.png"));
-		hoursPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[1], profile.getLanguage().ahmText[1], "AHM_1.png"));
-		hoursPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[2], profile.getLanguage().ahmText[2], "AHM_1.png"));
-		this.add(hoursPanel);
-		
-		this.add(createSpaceLabel(0));
-		
-		customPanel = new JPanel();
-		setPanelComponent(customPanel);
-		customPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[3], profile.getLanguage().ahmText[3], "AHM_1.png"));
-		customPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[4], profile.getLanguage().ahmText[4], "AHM_1.png"));
-		customPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[5], profile.getLanguage().ahmText[5], "AHM_1.png"));
-		this.add(customPanel);
-		
-		this.add(createSpaceLabel(0));
-		
-		maxPanel = new JPanel();
-		setPanelComponent(maxPanel);
-		maxPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[6], profile.getLanguage().ahmText[6], "AHM_1.png"));
-		maxPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[7], profile.getLanguage().ahmText[7], "AHM_1.png"));
-		maxPanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[8], profile.getLanguage().ahmText[8], "AHM_1.png"));
-		this.add(maxPanel);
-		
-		this.add(createSpaceLabel(0));
-		
-		timePanel = new JPanel();
-		setPanelComponent(timePanel);
-		timePanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[9], profile.getLanguage().ahmText[9], "AHM_1.png"));
-		timePanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[10], profile.getLanguage().ahmText[10], "AHM_1.png"));
-		timePanel.add(createAchievementLabel(
-				profile.getLanguage().ahmTitle[11], profile.getLanguage().ahmText[11], "AHM_1.png"));
-		this.add(timePanel);
-		
-		this.add(createSpaceLabel(0));
 	}
 
 	@Override
@@ -98,18 +56,72 @@ public class AchievementsStrategy extends StateStrategy {
 		
 	}
 	
+	public void setAchievements() {
+		hoursPanel = new JPanel();
+		setPanelComponent(hoursPanel);
+		hoursPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[0], profile.getLanguage().ahmText[0], "AHM_6.png", "AHM_6.png", earnedAchievements[0]));
+		hoursPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[1], profile.getLanguage().ahmText[1], "AHM_6.png", "AHM_6.png", earnedAchievements[1]));
+		hoursPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[2], profile.getLanguage().ahmText[2], "AHM_6.png", "AHM_6.png", earnedAchievements[2]));
+		this.add(hoursPanel);
+		
+		this.add(createSpaceLabel(0));
+		
+		customPanel = new JPanel();
+		setPanelComponent(customPanel);
+		customPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[3], profile.getLanguage().ahmText[3], "AHM_6.png", "AHM_6.png", earnedAchievements[3]));
+		customPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[4], profile.getLanguage().ahmText[4], "AHM_6.png", "AHM_6.png", earnedAchievements[4]));
+		customPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[5], profile.getLanguage().ahmText[5], "AHM_6.png", "AHM_6.png", earnedAchievements[5]));
+		this.add(customPanel);
+		
+		this.add(createSpaceLabel(0));
+		
+		maxPanel = new JPanel();
+		setPanelComponent(maxPanel);
+		maxPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[6], profile.getLanguage().ahmText[6], "AHM_6.png", "AHM_6A.png", earnedAchievements[6]));
+		maxPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[7], profile.getLanguage().ahmText[7], "AHM_6.png", "AHM_6A.png", earnedAchievements[7]));
+		maxPanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[8], profile.getLanguage().ahmText[8], "AHM_6.png", "AHM_6A.png", earnedAchievements[8]));
+		this.add(maxPanel);
+		
+		this.add(createSpaceLabel(0));
+		
+		timePanel = new JPanel();
+		setPanelComponent(timePanel);
+		timePanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[9], profile.getLanguage().ahmText[9], "AHM_6.png", "AHM_6A.png", earnedAchievements[9]));
+		timePanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[10], profile.getLanguage().ahmText[10], "AHM_6.png", "AHM_6A.png", earnedAchievements[10]));
+		timePanel.add(createAchievementLabel(
+				profile.getLanguage().ahmTitle[11], profile.getLanguage().ahmText[11], "AHM_6.png", "AHM_6A.png", earnedAchievements[11]));
+		this.add(timePanel);
+		
+		this.add(createSpaceLabel(0));
+	}
+	
 	public void setPanelComponent(JPanel panel) {
 		panel.setLayout(new GridLayout(1,3,30,30));
 		panel.setBackground(theme.subColor);
 		panel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 	}
 	
-	public JPanel createAchievementLabel(String title, String desc, String imageUrl) {
+	public JPanel createAchievementLabel(String title, String desc, String imageUrl, String imageUrlEarned, String indicator) {
 		JPanel panel = new JPanel();
 			panel.setBackground(theme.layerColor);
 			panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 			panel.setLayout(new BorderLayout());
-		JLabel imgLabel = new JLabel(new ImageIcon(getClass().getClassLoader().getResource(imageUrl)));
+		JLabel imgLabel;
+		if(indicator.equals("1"))
+			imgLabel = new JLabel(new ImageIcon(getClass().getClassLoader().getResource(imageUrlEarned)));
+		else
+			imgLabel = new JLabel(new ImageIcon(getClass().getClassLoader().getResource(imageUrl)));
 		
 		JPanel textPanel = new JPanel();
 			textPanel.setBackground(theme.layerColor);
