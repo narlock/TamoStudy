5862b26aaba00c7525ef1773c9f799d8248c1464
narlock <anthonynarlock@outlook.com>
2022-07-23 12:35:48 -0500

    TamoStream indev : Settings domain object

---
+++ TamoStudyStream/src/MainGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+/**
+ * MainGUI
+ * @author Anthony Narlock (narlock)
+ * @brief The Context and Main Frame of Program
+ */
+
+public class MainGUI extends JFrame {
+	
+	public MainGUI() {
+		
+	}
+}
--- TamoStudyStream/src/TamoStream.java
+++ TamoStudyStream/src/TamoStream.java
@@ -1,3 +1,8 @@
+/**
+ * TamoStream
+ * @author Anthony Narlock (narlock)
+ * @brief Runs the Main loop of the project
+ */
 
 public class TamoStream {
 	public static void main(String[] args) {
