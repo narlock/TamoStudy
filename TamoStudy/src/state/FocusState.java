0e33fcfd98817b0662c943c8cb032c1276cad071
Anthony <59890724+narlock@users.noreply.github.com>
2023-05-20 14:38:36 -0500

    Begin focus state

--- TamoStudy/src/state/FocusState.java
+++ TamoStudy/src/state/FocusState.java
@@ -1,12 +1,57 @@
 package state;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import components.panel.TamoGraphicsPanel;
 import gui.TamoStudyGUI;
+import model.GuiSize;
+import model.language.Language;
+import model.profile.Profile;
+import model.profile.Tamo;
+import model.time.DailyFocusEntry;
+import model.time.MonthFocusEntry;
+import resources.Theme;
 
 public class FocusState extends State {
 
 	private static final long serialVersionUID = -3878552282431644324L;
+	
+	/*
+	 * ##################################
+	 * ##################################
+	 * ATTRIBUTES
+	 * ##################################
+	 * ##################################
+	 */
+	private Profile profile;
+	private Language language;
+	private GuiSize guiSize;
+	private Tamo tamo;
+	private Theme theme;
+	private DailyFocusEntry dailyFocusEntry;
+	private MonthFocusEntry monthFocusEntry;
+	
+	/*
+	 * ##################################
+	 * ##################################
+	 * COMPONENTS
+	 * ##################################
+	 * ##################################
+	 */
+	
+	// Side 1
+	private JPanel tamoPanel;
+	
+	private JLabel tamoNameLabel;
+	private TamoGraphicsPanel tamoGraphicsPanel;
+	private JPanel tamoHappyHungerPanel;
+	private JLabel tamoHappyLabel;
+	private JLabel tamoHungerLabel;
+	
+	// Side 2
+	private JPanel timerPanel;
+	
 
 	public FocusState(TamoStudyGUI tamoStudyGUI) {
 		super(tamoStudyGUI);
